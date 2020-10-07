878464c28e59f6cb46ccb8b6f7d885ae89c00c42
Scratchytube <bpsahota@gmail.com>
2020-10-06 21:09:30 -0400

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,17 @@
+require 'pry'
+
 class Book
-    
+
     attr_accessor :author, :page_count, :genre
-    attr_reader  :title
+    attr_reader :title
 
     def initialize(title)
-        @title = title  
+        @title = title
     end
 
-   def turn_page
+    def turn_page
         puts "Flipping the page...wow, you read fast!"
     end
+
 
 end
